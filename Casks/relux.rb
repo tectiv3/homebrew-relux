799bef2d7c8696d409328770535e4f59e4c69a67
tectiv3 <git@getpanda.me>
2026-03-06 15:33:04 +0000

    Fix download URL to include v prefix in tag

--- Casks/relux.rb
+++ Casks/relux.rb
@@ -2,7 +2,7 @@ cask "relux" do
   version "1.7.1"
   sha256 "9b2c23ec97a074ed13878941ad6132d6fbc4a6970626199e326a0dbf6252b54c"
 
-  url "https://github.com/tectiv3/relux/releases/download/#{version}/Relux-#{version}.dmg"
+  url "https://github.com/tectiv3/relux/releases/download/v#{version}/Relux-#{version}.dmg"
   name "Relux"
   desc "macOS menu-bar utility for app launching, clipboard history, and translation"
   homepage "https://github.com/tectiv3/relux"
