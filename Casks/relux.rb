6ce6e0028a0857d6bd9c9b0793c68f3f26576fb2
tectiv3 <git@getpanda.me>
2026-03-06 15:26:49 +0000

    Add Relux cask v1.7.1

---
+++ Casks/relux.rb
@@ -0,0 +1,18 @@
+cask "relux" do
+  version "1.7.1"
+  sha256 "9b2c23ec97a074ed13878941ad6132d6fbc4a6970626199e326a0dbf6252b54c"
+
+  url "https://github.com/tectiv3/relux/releases/download/#{version}/Relux-#{version}.dmg"
+  name "Relux"
+  desc "macOS menu-bar utility for app launching, clipboard history, and translation"
+  homepage "https://github.com/tectiv3/relux"
+
+  depends_on macos: ">= :sonoma"
+
+  app "Relux.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.relux.app",
+    "~/Library/Preferences/com.relux.app.plist",
+  ]
+end
