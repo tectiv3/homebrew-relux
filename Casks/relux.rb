58a32577676cdabfb549508ffb45a2a309024f20
tectiv3 <git@getpanda.me>
2026-03-10 13:50:20 +0000

    Update Relux to 1.11.0

--- Casks/relux.rb
+++ Casks/relux.rb
@@ -1,6 +1,6 @@
 cask "relux" do
-  version "1.10.0"
-  sha256 "732bc756205ea8f75d52f81b2d582104915b942e0941efb830886aa7d3ae3759"
+  version "1.11.0"
+  sha256 "8754739dd023b44df5aa65999e9e909c9c858e39a84a7e37d80fc202596626ef"
 
   url "https://github.com/tectiv3/relux/releases/download/v#{version}/Relux-#{version}.dmg"
   name "Relux"
