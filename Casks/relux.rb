34e7e9c4063ffe3c19140dc41960141d8c6b4d1e
tectiv3 <tectiv3@users.noreply.github.com>
2026-09-15 16:30:23 +0200

    Update Relux to 1.18.0

--- Casks/relux.rb
+++ Casks/relux.rb
@@ -1,6 +1,6 @@
 cask "relux" do
-  version "1.17.1"
-  sha256 "2c2b8b3112126f9ec051aef2d5dd2ef82985a165a13db8c20a47fa8b333faa6f"
+  version "1.18.0"
+  sha256 "1a317c7d0d4813085c3671fca9c412718d11ad3b9df22e17074dac590021cff2"
 
   url "https://github.com/tectiv3/relux/releases/download/v#{version}/Relux-#{version}.dmg"
   name "Relux"
